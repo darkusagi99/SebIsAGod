f80a4fead0394fc4205d216a39a42b8cb8580558
darkusagi <darkusagi99@gmail.com>
2021-06-08 21:59:39 +0200

    Fonctionnalité applicative

--- app/src/main/java/com/gmail/darkusagi99/sebisagod/MainActivity.kt
+++ app/src/main/java/com/gmail/darkusagi99/sebisagod/MainActivity.kt
@@ -1,29 +1,54 @@
 package com.gmail.darkusagi99.sebisagod
 
 import android.os.Bundle
-import com.google.android.material.floatingactionbutton.FloatingActionButton
-import com.google.android.material.snackbar.Snackbar
-import androidx.appcompat.app.AppCompatActivity
-import android.view.Menu
-import android.view.MenuItem
+import android.speech.tts.TextToSpeech
 import android.widget.Button
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
+
+    private var tts : TextToSpeech? = null
+    private var ttsId = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        findViewById<Button>(R.id.SedBtn).setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action - Seb est un Dieu", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
+        tts = TextToSpeech(applicationContext) { status ->
+            if (status != TextToSpeech.ERROR) {
+                setTextToSpeechLanguage()
+            }
         }
 
-        findViewById<Button>(R.id.SatrBtn).setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action - Seb a toujours raison", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
+        findViewById<Button>(R.id.SedBtn).setOnClickListener {
+            tts!!.speak("Seb est un Dieu", TextToSpeech.QUEUE_FLUSH, null, ttsId.toString())
+            ttsId++
         }
+
+        findViewById<Button>(R.id.SatrBtn).setOnClickListener {
+            tts!!.speak("Seb a toujours raison", TextToSpeech.QUEUE_FLUSH, null, ttsId.toString())
+            ttsId++
+        }
+
     }
 
+    private fun setTextToSpeechLanguage() {
+        val language: Locale = Locale.FRENCH
+        val result  = tts?.setLanguage(language)
+        // pitch voice to 25%
+        tts?.setPitch(0.25F)
+        if (result == TextToSpeech.LANG_MISSING_DATA) {
+            Toast.makeText(this, "Missing language data", Toast.LENGTH_SHORT).show()
+            return
+        } else if (result == TextToSpeech.LANG_NOT_SUPPORTED) {
+            Toast.makeText(this, "Language not supported", Toast.LENGTH_SHORT).show()
+            return
+        } else {
+            Toast.makeText(this, "Langue FRANCAIS", Toast.LENGTH_SHORT).show()
+            return
+        }
+    }
 
 }
